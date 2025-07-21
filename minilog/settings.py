cddb54a02be2bc005da0df083ccb183fcff1b39e
Niyofabrice <nf.fabrice.niyonkuru@gmail.com>
2025-07-21 12:29:37 +0200

    added environment variables

--- minilog/settings.py
+++ minilog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&gglh&inbrzc!vf048tv-+66vaj=)t*3=0e7p1e%a@ljs_-h*k'
+SECRET_KEY = os.environ.get(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get(DEBUG)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get(ALLOWED_HOSTS).split(" ")
 
 
 # Application definition
