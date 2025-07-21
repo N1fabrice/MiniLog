c4b980df80ae38d95c3eab98a087c8a18b5ca9b1
Niyofabrice <nf.fabrice.niyonkuru@gmail.com>
2025-07-21 12:42:07 +0200

    changed environment variables

--- minilog/settings.py
+++ minilog/settings.py
@@ -21,13 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(SECRET_KEY)
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get(DEBUG)
+if ENVIRONMENT == 'development':
+    DEBUG = True
+else:
+    DEBUG = False
 
-ALLOWED_HOSTS = os.environ.get(ALLOWED_HOSTS).split(" ")
 
+ALLOWED_HOSTS = [ 'localhost', '127.0.0.1', 'minilog.up.railway.app' ]
+
+CSRF_TRUSTED_ORIGINS = [ 'https://minilog.up.railway.app/' ]
 
 # Application definition
 
